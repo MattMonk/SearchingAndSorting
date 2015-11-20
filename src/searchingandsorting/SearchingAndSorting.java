d38227fbe2d379e9738c8ab23b71d432461d494f
MON15150511 <MON15150511@R206-STUDPC-115.runshaw.net>
2015-11-20 10:59:21 +0000

    Comments.

--- src/searchingandsorting/SearchingAndSorting.java
+++ src/searchingandsorting/SearchingAndSorting.java
@@ -2,7 +2,7 @@ package searchingandsorting;
 
 public class SearchingAndSorting
 {
-    int[] cards = {3, 5, 4, 7 ,1};
+    private int[] cards = {3, 5, 4, 7 ,1};
     
     public void searchCard(int theCard)
     {
@@ -24,12 +24,13 @@ public class SearchingAndSorting
         {
             for(int k=0;k<cards.length-1;k++)
             {
-                if(cards[k]> cards[k+1])
+                if(cards[k]> cards[k+1])  //checks the cards in pairs and swaps if first > second, do this until k finds the right place
                 {
                     int tempCard = cards[k];
                     cards[k] = cards[k+1];
                     cards[k+1] = tempCard;
                 }
+                //then loops to do the same for every position in the array
             }
         }
     }
